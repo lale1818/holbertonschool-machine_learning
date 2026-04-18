8308e87d4fd7dd5f36cf24647d876c8985cc4d53
lale1818 <13583@holbertonstudents.com>
2026-04-18 18:25:29 +0000

    Fix: handled trailing zeros for [0] case

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -7,10 +7,6 @@ Module to calculate the integral of a polynomial
 def poly_integral(poly, C=0):
     """
     Calculates the integral of a polynomial
-    Args:
-        poly: list of coefficients
-        C: integration constant
-    Returns: list of coefficients of the integral
     """
     if not isinstance(poly, list) or len(poly) == 0:
         return None
@@ -26,9 +22,8 @@ def poly_integral(poly, C=0):
             value = int(value)
         integral.append(value)
 
-    if len(integral) > 1 and integral[-1] == 0:
-        if len(poly) > 1 and all(x == 0 for x in poly):
-            return [C]
-        # Bəzi testlər üçün sadəcə [C, 0] qaytarmaq lazım ola bilər, 
-        # amma standart həll budur.
+    # Sondakı sıfırları təmizləyirik ki, [0] və ya [7] kimi nəticə qalsın
+    while len(integral) > 1 and integral[-1] == 0:
+        integral.pop()
+
     return integral
