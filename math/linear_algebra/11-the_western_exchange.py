2f2fa732a3b570754cf9e0444eca9711f63e9902
lale1818 <13583@holbertonstudents.com>
2026-04-25 08:35:28 +0000

    Add Task 11: NumPy transpose function

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module to transpose a numpy.ndarray
+"""
+import numpy as np
+
+
+def np_transpose(matrix):
+    """
+    Transposes a numpy.ndarray
+    """
+    return matrix.transpose()
