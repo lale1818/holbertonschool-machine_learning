ff9aca362116a1c81bd73421e045f09eba364304
lale1818 <13583@holbertonstudents.com>
2026-04-25 08:38:44 +0000

    Add Task 12: Element-wise operations with NumPy

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module to perform element-wise operations on numpy.ndarrays
+"""
+
+
+def np_elementwise(mat1, mat2):
+    """
+    Performs element-wise addition, subtraction, multiplication, and division
+    """
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
