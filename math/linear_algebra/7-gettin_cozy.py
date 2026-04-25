26e6968042c7a62486afd02255462d62c196b8e2
lale1818 <13583@holbertonstudents.com>
2026-04-25 08:29:28 +0000

    Add Task 7: 2D matrix concatenation along axis

---
+++ math/linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Module to concatenate 2D matrices along a specific axis
+"""
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """
+    Concatenates two 2D matrices along a specific axis
+    """
+    if axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
+        return [row[:] for row in mat1] + [row[:] for row in mat2]
+
+    if axis == 1:
+        if len(mat1) != len(mat2):
+            return None
+        return [mat1[i] + mat2[i] for i in range(len(mat1))]
+
+    return None
