30c4b68267592596bc8d5ac7d820eb97c5eec26a
lale1818 <13583@holbertonstudents.com>
2026-05-01 21:54:22 +0000

    Fix pycodestyle W293 whitespace errors in task 5

--- math/advanced_linear_algebra/5-definiteness.py
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -11,17 +11,16 @@ def definiteness(matrix):
     """
     if not isinstance(matrix, np.ndarray):
         raise TypeError("matrix must be a numpy.ndarray")
-    
+
     if len(matrix.shape) != 2 or matrix.shape[0] != matrix.shape[1]:
         return None
-    
-    # Definiteness is typically defined for symmetric matrices
+
     if not np.allclose(matrix, matrix.T):
         return None
 
     try:
         eigenvalues = np.linalg.eigvals(matrix)
-        
+
         if np.all(eigenvalues > 0):
             return "Positive definite"
         if np.all(eigenvalues < 0):
@@ -30,7 +29,7 @@ def definiteness(matrix):
             return "Positive semi-definite"
         if np.all(eigenvalues <= 0):
             return "Negative semi-definite"
-        
+
         pos = any(eigenvalues > 0)
         neg = any(eigenvalues < 0)
         if pos and neg:
