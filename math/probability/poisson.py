d2447373afd3d922ffab3387c0e198f74655b9db
lale1818 <13583@holbertonstudents.com>
2026-05-09 18:11:58 +0000

    Initialize Poisson class with lambtha calculation

---
+++ math/probability/poisson.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Module that represents a poisson distribution
+"""
+
+
+class Poisson:
+    """
+    Class Poisson that represents a poisson distribution
+    """
+
+    def __init__(self, data=None, lambtha=1.):
+        """
+        Initialize Poisson distribution
+        """
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = float(sum(data) / len(data))
