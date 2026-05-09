405da2f1cce4e5bb6d32ba6b599a73bac4324905
lale1818 <13583@holbertonstudents.com>
2026-05-09 18:35:59 +0000

    Add task 8: Normal PDF

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -28,3 +28,16 @@ class Normal:
     def x_value(self, z):
         """Calculates the x-value of a given z-score"""
         return self.mean + (z * self.stddev)
+
+    def pdf(self, x):
+        """Calculates the value of the PDF for a given x-value"""
+        pi = 3.1415926536
+        e = 2.7182818285
+
+        # z = (x - mu) / sigma
+        z = self.z_score(x)
+
+        exponent = -0.5 * (z ** 2)
+        coefficient = 1 / (self.stddev * ((2 * pi) ** 0.5))
+
+        return coefficient * (e ** exponent)
