e0e28c466b02a9a62c76d00fab14af788e1b7854
lale1818 <13583@holbertonstudents.com>
2026-05-09 18:39:43 +0000

    Add task 12: Binomial CDF

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -40,11 +40,23 @@ class Binomial:
                 res *= i
             return res
 
-        # n! / (k! * (n - k)!)
         n_fact = fact(self.n)
         k_fact = fact(k)
         nk_fact = fact(self.n - k)
         combination = n_fact / (k_fact * nk_fact)
 
-        # P(k) = combination * p^k * (1-p)^(n-k)
         return combination * (self.p ** k) * ((1 - self.p) ** (self.n - k))
+
+    def cdf(self, k):
+        """Calculates the value of the CDF for a given number of successes"""
+        if not isinstance(k, int):
+            k = int(k)
+        if k < 0:
+            return 0
+        if k > self.n:
+            return 1
+
+        cdf_val = 0
+        for i in range(k + 1):
+            cdf_val += self.pmf(i)
+        return cdf_val
