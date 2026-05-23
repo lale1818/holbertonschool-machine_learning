177b3e394deb21a5a0e143f6b491405c6824cf55
lale1818 <13583@holbertonstudents.com>
2026-05-23 19:44:24 +0000

    Add Task 13: Compute descriptive statistics using describe excluding Timestamp

---
+++ pipeline/pandas/13-analyze.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Computes descriptive statistics for a DataFrame
+"""
+import pandas as pd
+
+
+def analyze(df):
+    """ Computes descriptive statistics for all columns except Timestamp """
+    return df.drop(columns=['Timestamp']).describe()
