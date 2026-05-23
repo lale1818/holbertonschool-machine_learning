0e7684e835830eb9b6249868b9851907b2f8348b
lale1818 <lale1818@github.com>
2026-05-23 19:55:39 +0000

    Fix Task 4: Simplify docstrings for regex checker

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 """
-Defines a function that converts parts of a pd.DataFrame into a numpy.ndarray
+Defines array function
 """
 import pandas as pd
 
 
 def array(df):
     """
-    Selects the last 10 rows of High and Close columns
-    and converts them into a numpy.ndarray
+    Converts columns to ndarray
     """
     return df[['High', 'Close']].tail(10).to_numpy()
