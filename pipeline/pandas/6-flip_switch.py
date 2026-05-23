1cd33df15e7e8794d3264cda87a2d7119f9ffe15
lale1818 <lale1818@github.com>
2026-05-23 19:57:24 +0000

    Fix Task 6: Clean code and formatting

--- pipeline/pandas/6-flip_switch.py
+++ pipeline/pandas/6-flip_switch.py
@@ -2,12 +2,10 @@
 """
 Defines flip_switch function
 """
-import pandas as pd
 
 
 def flip_switch(df):
     """
     Sorts dataframe in reverse chronological order and transposes it
     """
-    # .iloc[::-1] sətirləri tərs çevirir, .T isə transpose edir
     return df.iloc[::-1].T
