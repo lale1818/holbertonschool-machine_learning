b39e9c7d68854ae507390d290b609e82c10ccbf2
lale1818 <lale1818@github.com>
2026-05-23 19:50:58 +0000

    Add Task 3: Rename column, convert to datetime, and select specific columns

--- pipeline/pandas/3-rename.py
+++ pipeline/pandas/3-rename.py
@@ -1,20 +1,15 @@
 #!/usr/bin/env python3
 """
-Modifies a DataFrame by renaming columns and converting time formats.
+Renames a column and converts its values to datetime format
 """
 import pandas as pd
 
 
 def rename(df):
     """
-    Renames Timestamp column to Datetime, converts it to datetime objects,
-    and modifies the DataFrame to keep only Datetime and Close columns.
+    Renames Timestamp to Datetime, converts values to datetime,
+    and returns only Datetime and Close columns.
     """
-    df.rename(columns={'Timestamp': 'Datetime'}, inplace=True)
-    
+    df = df.rename(columns={'Timestamp': 'Datetime'})
     df['Datetime'] = pd.to_datetime(df['Datetime'], unit='s')
-    
-    columns_to_drop = [col for col in df.columns if col not in ['Datetime', 'Close']]
-    df.drop(columns=columns_to_drop, inplace=True)
-    
-    return df
+    return df[['Datetime', 'Close']]
