237f4b9b61b1606de16f6334389dfc0cfbe7e433
lale1818 <13583@holbertonstudents.com>
2026-05-29 11:33:29 +0000

    Add Task 5: Combine all 5 plots into a 3x2 layout with a shared figure title

---
+++ math/plotting/5-all_in_one.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+"""
+Plots all 5 previous graphs in one figure with a 3x2 grid.
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def all_in_one():
+    """ Combines 5 plots into a single layout with custom sizing and labels """
+    y0 = np.arange(0, 11) ** 3
+
+    mean = [69, 0]
+    cov = [[15, 8], [8, 15]]
+    np.random.seed(5)
+    x1, y1 = np.random.multivariate_normal(mean, cov, 2000).T
+    y1 += 180
+
+    x2 = np.arange(0, 28651, 5730)
+    r2 = np.log(0.5)
+    t2 = 5730
+    y2 = np.exp((r2 / t2) * x2)
+
+    x3 = np.arange(0, 21000, 1000)
+    r3 = np.log(0.5)
+    t31 = 5730
+    t32 = 1600
+    y31 = np.exp((r3 / t31) * x3)
+    y32 = np.exp((r3 / t32) * x3)
+
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+
+    plt.figure(figsize=(6.4, 4.8))
+
+    # Task 0
+    plt.subplot(3, 2, 1)
+    plt.plot(np.arange(0, 11), y0, 'r-')
+    plt.xlim(0, 10)
+
+    # Task 1
+    plt.subplot(3, 2, 2)
+    plt.scatter(x1, y1, color='m', s=1)
+    plt.xlabel('Height (in)', fontsize='x-small')
+    plt.ylabel('Weight (lbs)', fontsize='x-small')
+    plt.title("Men's Height vs Weight", fontsize='x-small')
+
+    # Task 2
+    plt.subplot(3, 2, 3)
+    plt.plot(x2, y2)
+    plt.xlabel('Time (years)', fontsize='x-small')
+    plt.ylabel('Fraction Remaining', fontsize='x-small')
+    plt.title('Exponential Decay of C-14', fontsize='x-small')
+    plt.yscale('log')
+    plt.xlim(0, 28650)
+
+    # Task 3
+    plt.subplot(3, 2, 4)
+    plt.plot(x3, y31, 'r--', label='C-14')
+    plt.plot(x3, y32, 'g-', label='Ra-226')
+    plt.xlabel('Time (years)', fontsize='x-small')
+    plt.ylabel('Fraction Remaining', fontsize='x-small')
+    plt.title('Exponential Decay of Radioactive Elements', fontsize='x-small')
+    plt.xlim(0, 20000)
+    plt.ylim(0, 1)
+    plt.legend(loc='upper right', fontsize='x-small')
+
+    # Task 4
+    plt.subplot(3, 1, 3)
+    bins = list(range(40, 101, 10))
+    plt.hist(student_grades, bins=bins, edgecolor='black')
+    plt.xlabel('Grades', fontsize='x-small')
+    plt.ylabel('Number of Students', fontsize='x-small')
+    plt.title('Project A', fontsize='x-small')
+    plt.xlim(40, 100)
+    plt.ylim(0, 30)
+
+    plt.suptitle('All in One')
+    plt.tight_layout()
+    plt.show()
