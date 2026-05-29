b0eae437ec9e86a9c981da1894eddfdf3cd2cc72
lale1818 <13583@holbertonstudents.com>
2026-05-29 11:32:19 +0000

    Fix Task 4: Set explicit xlim from 40 to 100 to match reference image

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -7,15 +7,16 @@ import numpy as np
 
 
 def frequency():
-    """ Plots a histogram with bins every 10 units from 40 to 100 """
+    """ Plots a histogram with 40-100 bins and explicit 40-100 x-limits """
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
 
-    bins = np.arange(40, 101, 10)
+    bins = list(range(40, 101, 10))
     plt.hist(student_grades, bins=bins, edgecolor='black')
     
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
+    plt.xlim(40, 100)
     plt.show()
