a0de9c87dba0a2c8c188c327bc3fb3566fbc5528
lale1818 <13583@holbertonstudents.com>
2026-05-29 11:31:27 +0000

    Fix Task 4: Match reference plot by adjusting bins and removing xlim

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -7,16 +7,15 @@ import numpy as np
 
 
 def frequency():
-    """ Plots a histogram with bins every 10 units and black outlines """
+    """ Plots a histogram with bins every 10 units from 40 to 100 """
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
 
-    bins = np.arange(0, 101, 10)
+    bins = np.arange(40, 101, 10)
     plt.hist(student_grades, bins=bins, edgecolor='black')
     
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
-    plt.xlim(0, 100)
     plt.show()
