e9678515be3982b3a40cb9ac721920ec4d02d02c
lale1818 <13583@holbertonstudents.com>
2026-05-29 11:41:38 +0000

    Fix Task 4: Use 0-100 arange for bins without explicit xlim to match reference exactly

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -7,16 +7,15 @@ import numpy as np
 
 
 def frequency():
-    """ Plots a histogram with 40-100 bins and explicit 40-100 x-limits """
+    """ Plots a histogram with 0-100 bins based on 10 units step """
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
 
-    bins = list(range(40, 101, 10))
+    bins = np.arange(0, 101, 10)
     plt.hist(student_grades, bins=bins, edgecolor='black')
     
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
-    plt.xlim(40, 100)
     plt.show()
