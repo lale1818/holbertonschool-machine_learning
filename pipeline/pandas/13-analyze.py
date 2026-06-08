58dabc3a2afebb21c45bdd6fcc70895946449d17
lale1818 <13583@holbertonstudents.com>
2026-06-08 21:57:29 +0000

    Fix task 13: Clean up syntax and comments to pass pycodestyle and checker

--- pipeline/pandas/13-analyze.py
+++ pipeline/pandas/13-analyze.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 """Defines a function to compute descriptive statistics of a DataFrame"""
-import pandas as pd
 
 
 def analyze(df):
     """
     Computes descriptive statistics for all columns except Timestamp.
     """
-    # Timestamp sütunu çıxmaqla statistika hesablayırıq
     return df.drop(columns=['Timestamp'], errors='ignore').describe()
