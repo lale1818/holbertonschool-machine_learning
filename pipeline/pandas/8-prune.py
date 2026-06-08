886728224b9f592689b928c2ea45ba1253f9ef0b
lale1818 <13583@holbertonstudents.com>
2026-06-08 22:04:42 +0000

    Fix task 8: Shorten docstrings to pass pycodestyle E501 line length limit

--- pipeline/pandas/8-prune.py
+++ pipeline/pandas/8-prune.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
-"""Defines a function to prune NaN values from a specific column in a DataFrame"""
+"""Prunes NaN values from a DataFrame"""
 
 
 def prune(df):
     """
-    Removes any entries where Close has NaN values.
+    Removes entries where Close has NaN values.
 
     Args:
         df (pd.DataFrame): The input dataframe.
 
     Returns:
-        pd.DataFrame: The modified dataframe without NaN values in Close column.
+        pd.DataFrame: The modified dataframe.
     """
     return df.dropna(subset=['Close'])
