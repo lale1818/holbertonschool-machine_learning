aed7bcb003774164e43ee5437853773ca75398d9
lale1818 <13583@holbertonstudents.com>
2026-06-08 21:55:21 +0000

    Add task 11: Concatenate filtered Bitstamp data on top of Coinbase data

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -1,23 +1,28 @@
 #!/usr/bin/env python3
-"""
-Defines concat function
-"""
+"""Defines a function to concatenate two DataFrames with specific indexing and keys"""
 import pandas as pd
 index = __import__('10-index').index
 
 
 def concat(df1, df2):
     """
-    Concatenates two dataframes after indexing and filtering
+    Concatenates specific rows of df2 to the top of df1 after indexing.
+
+    Args:
+        df1 (pd.DataFrame): Coinbase data.
+        df2 (pd.DataFrame): Bitstamp data.
+
+    Returns:
+        pd.DataFrame: The concatenated dataframe.
     """
-    df1 = index(df1)
-    df2 = index(df2)
+    # Hər iki DataFrame-i Timestamp sütununa görə indeksləyirik
+    df1_indexed = index(df1)
+    df2_indexed = index(df2)
 
-    df2_filtered = df2.loc[:1417411920]
+    # df2-ni timestamp 1417411920-yə qədər (daxil olmaqla) filtrləyirik
+    df2_filtered = df2_indexed.loc[:1417411920]
 
-    res = pd.concat([df2_filtered, df1], keys=['bitstamp', 'coinbase'])
-    
-    # Checker-in birbaşa .loc[1417411920] tapması üçün MultiIndex-i sığortalayırıq
-    res.index.set_names(['Source', 'Timestamp'], inplace=True)
-    
-    return res
+    # df2_filtered hissəsini df1-in üstünə birləşdiririk və key-ləri veririk
+    result = pd.concat([df2_filtered, df1_indexed], keys=['bitstamp', 'coinbase'])
+
+    return result
