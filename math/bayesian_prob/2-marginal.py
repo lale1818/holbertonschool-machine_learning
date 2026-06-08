62c10b210fc2c5d9458f9dd89a19354f7d033b53
lale1818 <13583@holbertonstudents.com>
2026-06-08 21:13:09 +0000

    Add tasks 2 and 3: Implement marginal and posterior probability calculations

---
+++ math/bayesian_prob/2-marginal.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""Calculates the marginal probability of obtaining data"""
+import numpy as np
+
+
+def marginal(x, n, P, Pr):
+    """
+    Calculates the marginal probability of obtaining data x and n.
+    """
+    if type(n) is not int or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if type(x) is not int or x < 0:
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0"
+        )
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+    if np.any(P < 0) or np.any(P > 1):
+        raise ValueError("All values in P must be in the range [0, 1]")
+    if np.any(Pr < 0) or np.any(Pr > 1):
+        raise ValueError("All values in Pr must be in the range [0, 1]")
+    if not np.isclose(np.sum(Pr), 1):
+        raise ValueError("Pr must sum to 1")
+
+    # Likelihood hesablanması
+    fact_n = np.math.factorial(n)
+    fact_x = np.math.factorial(x)
+    fact_nx = np.math.factorial(n - x)
+    combination = fact_n / (fact_x * fact_nx)
+    like = combination * (P ** x) * ((1 - P) ** (n - x))
+
+    # Kəsişmə (Intersection) massivi
+    intersect = like * Pr
+
+    # Marjinal ehtimal kəsişmələrin cəmidir
+    return np.sum(intersect)
