fe0ee9d66759b195f0ad22895df67b4ea0b815bb
lale1818 <13583@holbertonstudents.com>
2026-07-07 19:01:52 +0000

    Fix Task 9: Initialize NeuralNetwork with private attributes and properties

--- supervised_learning/classification/9-neural_network.py
+++ supervised_learning/classification/9-neural_network.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 This module defines a neural network with one hidden layer
-performing binary classification.
+performing binary classification with private attributes and getters.
 """
 import numpy as np
 
@@ -13,7 +13,7 @@ class NeuralNetwork:
 
     def __init__(self, nx, nodes):
         """
-        Initializes the neural network.
+        Initializes the neural network with private attributes.
         """
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
@@ -60,33 +60,3 @@ class NeuralNetwork:
     def A2(self):
         """ Getter for private activated output __A2 """
         return self.__A2
-
-    def forward_prop(self, X):
-        """
-        Calculates the forward propagation of the neural network.
-        """
-        Z1 = np.dot(self.__W1, X) + self.__b1
-        self.__A1 = 1 / (1 + np.exp(-Z1))
-
-        Z2 = np.dot(self.__W2, self.__A1) + self.__b2
-        self.__A2 = 1 / (1 + np.exp(-Z2))
-
-        return self.__A1, self.__A2
-
-    def cost(self, Y, A):
-        """
-        Calculates the cost of the model using logistic regression.
-        """
-        m = Y.shape[1]
-        loss = -(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
-        cost = np.sum(loss) / m
-        return cost
-
-    def evaluate(self, X, Y):
-        """
-        Evaluates the neural network's predictions.
-        """
-        _, A2 = self.forward_prop(X)
-        cost = self.cost(Y, A2)
-        prediction = np.where(A2 >= 0.5, 1, 0)
-        return prediction, cost
