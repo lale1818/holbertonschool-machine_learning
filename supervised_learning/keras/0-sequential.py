3d7d5d1485e3df46f081ce6ab55fae04532790f0
lale1818 <13583@holbertonstudents.com>
2026-07-18 19:51:50 +0000

    Fix Task 0: Correct directory structure and full code implementation

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -20,24 +20,20 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
 
     for i in range(len(layers)):
         if i == 0:
-            model.add(
-                K.layers.Dense(
-                    layers[i],
-                    activation=activations[i],
-                    kernel_regularizer=regularizer,
-                    input_shape=(nx,)
-                )
-            )
+            model.add(K.layers.Dense(
+                layers[i],
+                activation=activations[i],
+                kernel_regularizer=regularizer,
+                input_shape=(nx,)
+            ))
         else:
-            model.add(
-                K.layers.Dense(
-                    layers[i],
-                    activation=activations[i],
-                    kernel_regularizer=regularizer
-                )
-            )
+            model.add(K.layers.Dense(
+                layers[i],
+                activation=activations[i],
+                kernel_regularizer=regularizer
+            ))
 
-        if i != len(layers) - 1:
+        if i < len(layers) - 1:
             model.add(K.layers.Dropout(1 - keep_prob))
 
     return model
