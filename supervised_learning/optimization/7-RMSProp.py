06d232903a6b983b6287344c6e74bb7f7c370300
lale1818 <13583@holbertonstudents.com>
2026-08-07 20:22:42 +0000

    Fix Task 7: Implement update_variables_RMSProp function

---
+++ supervised_learning/optimization/7-RMSProp.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+This module contains a function that updates a variable using
+the RMSProp optimization algorithm.
+"""
+
+
+def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
+    """
+    Updates a variable using the RMSProp optimization algorithm.
+
+    Parameters:
+    - alpha: learning rate
+    - beta2: RMSProp weight
+    - epsilon: small number to avoid division by zero
+    - var: numpy.ndarray containing the variable to be updated
+    - grad: numpy.ndarray containing the gradient of var
+    - s: previous second moment of var
+
+    Returns:
+    - updated variable and the new moment, respectively
+    """
+    s_new = beta2 * s + (1 - beta2) * (grad ** 2)
+    var_updated = var - alpha * (grad / (s_new ** 0.5 + epsilon))
+
+    return var_updated, s_new
