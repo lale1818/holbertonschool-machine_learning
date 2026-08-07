31f5aee564037673407c34b97933f2c68e11a2a6
lale1818 <13583@holbertonstudents.com>
2026-08-07 20:24:45 +0000

    Fix Task 10: Implement create_Adam_op function

---
+++ supervised_learning/optimization/10-Adam.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+This module contains a function that sets up the Adam optimization
+algorithm in TensorFlow.
+"""
+import tensorflow as tf
+
+
+def create_Adam_op(alpha, beta1, beta2, epsilon):
+    """
+    Sets up the Adam optimization algorithm in TensorFlow.
+
+    Parameters:
+    - alpha: learning rate
+    - beta1: weight used for the first moment
+    - beta2: weight used for the second moment
+    - epsilon: small number to avoid division by zero
+
+    Returns:
+    - optimizer: Adam optimizer instance
+    """
+    return tf.keras.optimizers.Adam(
+        learning_rate=alpha,
+        beta_1=beta1,
+        beta_2=beta2,
+        epsilon=epsilon
+    )
