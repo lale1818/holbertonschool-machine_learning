2cd45824096b2c46d1d00987cfa1763d4bea2fba
lale1818 <13583@holbertonstudents.com>
2026-08-07 20:59:51 +0000

    Task 14: Implement create_batch_norm_layer

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """
-This module contains a function that creates a batch normalization layer
-for a neural network in TensorFlow.
+Module to create a batch normalization layer in TensorFlow.
 """
 import tensorflow as tf
 
@@ -13,18 +12,22 @@ def create_batch_norm_layer(prev, n, activation):
     Parameters:
     - prev: activated output of the previous layer
     - n: number of nodes in the layer to be created
-    - activation: activation function to be used on the output of the layer
+    - activation: activation function to be used on the output
 
     Returns:
-    - a tensor of the activated output for the layer
+    - Tensor of the activated output for the layer
     """
     init = tf.keras.initializers.VarianceScaling(mode='fan_avg')
-    dense = tf.keras.layers.Dense(units=n, kernel_initializer=init)(prev)
+    dense = tf.keras.layers.Dense(
+        units=n,
+        kernel_initializer=init,
+        use_bias=False
+    )(prev)
 
     batch_norm = tf.keras.layers.BatchNormalization(
+        epsilon=1e-7,
         gamma_initializer='ones',
-        beta_initializer='zeros',
-        epsilon=1e-7
+        beta_initializer='zeros'
     )(dense)
 
     if activation is None:
