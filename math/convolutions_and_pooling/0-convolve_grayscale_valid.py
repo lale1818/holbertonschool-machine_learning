f74d075de2fddb3991056c8f53e5bffa9d073a8c
lale1818 <13583@holbertonstudents.com>
2026-08-07 20:44:33 +0000

    Task 0: Implement convolve_grayscale_valid

---
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Module to perform a valid convolution on grayscale images.
+"""
+import numpy as np
+
+
+def convolve_grayscale_valid(images, kernel):
+    """
+    Performs a valid convolution on grayscale images.
+
+    Parameters:
+    - images: numpy.ndarray of shape (m, h, w) containing multiple images
+    - kernel: numpy.ndarray of shape (kh, kw) containing convolution kernel
+
+    Returns:
+    - numpy.ndarray containing the convolved images
+    """
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+
+    out_h = h - kh + 1
+    out_w = w - kw + 1
+
+    convolved = np.zeros((m, out_h, out_w))
+
+    for i in range(out_h):
+        for j in range(out_w):
+            image_slice = images[:, i:i + kh, j:j + kw]
+            convolved[:, i, j] = np.sum(image_slice * kernel, axis=(1, 2))
+
+    return convolved
