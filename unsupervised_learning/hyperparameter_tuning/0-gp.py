c9a040e39c5c71d761a33c10977f45f4a789bace
lale1818 <13583@holbertonstudents.com>
2026-08-26 09:59:01 +0000

    Initialize GaussianProcess class with RBF kernel

---
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Gaussian Process module
+"""
+import numpy as np
+
+
+class GaussianProcess:
+    """
+    Represents a noiseless 1D Gaussian process
+    """
+
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+        """
+        Constructor for GaussianProcess
+
+        Parameters:
+            X_init: numpy.ndarray of shape (t, 1) containing inputs sampled
+            Y_init: numpy.ndarray of shape (t, 1) containing outputs for X_init
+            l: length parameter for the kernel
+            sigma_f: standard deviation given to output of black-box function
+        """
+        self.X = X_init
+        self.Y = Y_init
+        self.l = l
+        self.sigma_f = sigma_f
+        self.K = self.kernel(X_init, X_init)
+
+    def kernel(self, X1, X2):
+        """
+        Calculates the covariance kernel matrix between two matrices
+        using the Radial Basis Function (RBF) kernel
+
+        Parameters:
+            X1: numpy.ndarray of shape (m, 1)
+            X2: numpy.ndarray of shape (n, 1)
+
+        Returns:
+            Covariance kernel matrix of shape (m, n)
+        """
+        sqdist = np.sum(X1**2, 1).reshape(-1, 1) + np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T)
+        return self.sigma_f ** 2 * np.exp(-0.5 / self.l ** 2 * sqdist)
