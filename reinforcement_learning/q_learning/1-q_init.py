34ce0c356691b97c4a3d4c2239680d640dcb8a1b
lale1818 <13583@holbertonstudents.com>
2026-08-26 11:48:09 +0000

    Add Task 1: Initialize Q-table

---
+++ reinforcement_learning/q_learning/1-q_init.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Module to initialize the Q-table for the environment.
+"""
+import numpy as np
+
+
+def q_init(env):
+    """
+    Initializes the Q-table with zeros.
+
+    Parameters:
+    - env is the FrozenLakeEnv instance
+
+    Returns:
+    - the Q-table as a numpy.ndarray of zeros
+    """
+    action_space_size = env.action_space.n
+    state_space_size = env.observation_space.n
+
+    return np.zeros((state_space_size, action_space_size))
