68379fe4b6df73f45bbe2268415a5c450d3e7719
lale1818 <13583@holbertonstudents.com>
2026-08-26 10:38:42 +0000

    fllf

--- supervised_learning/keras/9-model.py
+++ supervised_learning/keras/9-model.py
@@ -2,32 +2,31 @@
 """
 Save and Load Keras Model
 """
-import tensorflow as tf
+import tensorflow.keras as K
 
 
 def save_model(network, filename):
     """
     Saves an entire model to a file.
-    
+
     Args:
         network: The model to save.
-        filename (str): The path of the file that the model should be saved to.
-        
+        filename: Path of the file that the model should be saved to.
+
     Returns:
         None
     """
     network.save(filename)
-    return None
 
 
 def load_model(filename):
     """
     Loads an entire model from a file.
-    
+
     Args:
-        filename (str): The path of the file that the model should be loaded from.
-        
+        filename: Path of the file that the model should be loaded from.
+
     Returns:
         The loaded model.
     """
-    return tf.keras.models.load_model(filename)
+    return K.models.load_model(filename)
