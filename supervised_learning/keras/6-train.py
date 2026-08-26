f391a3167308831815c478334bccec11ecc96fd6
lale1818 <13583@holbertonstudents.com>
2026-08-26 10:21:59 +0000

    Add train_model function with early stopping

--- supervised_learning/keras/6-train.py
+++ supervised_learning/keras/6-train.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Model training module with Early Stopping using Keras
+Trains a Keras model using early stopping.
 """
 import tensorflow.keras as K
 
@@ -9,31 +9,31 @@ def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, early_stopping=False,
                 patience=0, verbose=True, shuffle=False):
     """
-    Trains a model using mini-batch gradient descent and optionally
-    uses Early Stopping based on validation loss
+    Trains a model using mini-batch gradient descent with early stopping.
 
     Parameters:
-        network: the model to train
-        data: numpy.ndarray of shape (m, nx) containing input data
-        labels: one-hot numpy.ndarray of shape (m, classes) containing labels
-        batch_size: size of the batch used for mini-batch gradient descent
-        epochs: number of passes through data
-        validation_data: tuple of (X_valid, Y_valid) to validate model with
-        early_stopping: boolean indicating whether early stopping should be used
-        patience: patience used for early stopping
-        verbose: boolean that determines if output should be printed
-        shuffle: boolean that determines whether to shuffle batches each epoch
+    - network: the model to train
+    - data: numpy.ndarray of shape (m, nx) containing input data
+    - labels: one-hot numpy.ndarray of shape (m, classes) containing labels
+    - batch_size: size of the batch used for mini-batch gradient descent
+    - epochs: number of passes through data
+    - validation_data: tuple (X_valid, Y_valid) for validation data
+    - early_stopping: boolean indicating whether to use early stopping
+    - patience: patience count used for early stopping
+    - verbose: boolean determining whether to output during training
+    - shuffle: boolean determining whether to shuffle batches every epoch
 
     Returns:
-        the History object generated after training the model
+    - History object generated after training the model
     """
     callbacks = []
-    if early_stopping and validation_data is not None:
-        early_stop_callback = K.callbacks.EarlyStopping(
+
+    if validation_data and early_stopping:
+        early_stop = K.callbacks.EarlyStopping(
             monitor='val_loss',
             patience=patience
         )
-        callbacks.append(early_stop_callback)
+        callbacks.append(early_stop)
 
     history = network.fit(
         x=data,
@@ -45,4 +45,5 @@ def train_model(network, data, labels, batch_size, epochs,
         verbose=verbose,
         shuffle=shuffle
     )
+
     return history
