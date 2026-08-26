c0f1820cef6c460b00c92b6ed6280480b79b216f
lale1818 <13583@holbertonstudents.com>
2026-08-26 11:44:06 +0000

    Add Task 0 and README.md for q_learning

---
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Module to load the FrozenLake environment from Gymnasium.
+"""
+import gymnasium as gym
+
+
+def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
+    """
+    Loads the pre-made FrozenLakeEnv environment from gymnasium.
+
+    Parameters:
+    - desc: list of lists containing a custom description of the map
+    - map_name: string containing the pre-made map to load
+    - is_slippery: boolean determining if the ice is slippery
+
+    Returns:
+    - the gymnasium environment
+    """
+    if desc is None and map_name is None:
+        env = gym.make('FrozenLake-v1', map_name='8x8', is_slippery=is_slippery)
+    elif desc is not None:
+        env = gym.make('FrozenLake-v1', desc=desc, is_slippery=is_slippery)
+    else:
+        env = gym.make('FrozenLake-v1', map_name=map_name, is_slippery=is_slippery)
+
+    return env
---
+++ reinforcement_learning/q_learning/README.md
@@ -0,0 +1,6 @@
+# Q-Learning
+
+This project covers Reinforcement Learning concepts and Q-Learning algorithms using Gymnasium.
+
+## Tasks
+* **0. Load the Environment**: `0-load_env.py` - Loads the FrozenLake environment from Gymnasium.
