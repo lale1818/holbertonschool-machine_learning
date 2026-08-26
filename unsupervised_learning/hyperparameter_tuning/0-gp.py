22ae6a5309f66d0dacbc1be17b3e9c6c66f6038f
lale1818 <13583@holbertonstudents.com>
2026-08-26 10:02:25 +0000

    Fix W504 line break issue in 0-gp.py

--- unsupervised_learning/hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -38,6 +38,6 @@ class GaussianProcess:
         Returns:
             Covariance kernel matrix of shape (m, n)
         """
-        sqdist = (np.sum(X1 ** 2, 1).reshape(-1, 1) +
-                  np.sum(X2 ** 2, 1) - 2 * np.dot(X1, X2.T))
+        sqdist = (np.sum(X1 ** 2, 1).reshape(-1, 1)
+                  + np.sum(X2 ** 2, 1) - 2 * np.dot(X1, X2.T))
         return self.sigma_f ** 2 * np.exp(-0.5 / self.l ** 2 * sqdist)
